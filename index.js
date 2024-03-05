a5b2e04e4bb751193f2373bbd26ac1b707605e54
GuiBrumz <guibrumbatista@gmail.com>
2024-03-05 11:32:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// comando para exibir mensagem
+console.log("O GREMIO VAI SAIR CAMPEAO E O INTER NAO!!!")
+
+// exercicio de calculos 
+
+const num1 = 20;
+const num2 = 654;
+
+// soma
+let soma = num1 + num2;
+console. log("O valor da soma é " + soma + " daleeeeeeeeee gremio");
+
+let subtração = num1 - num2;
+console. log("O valor da subtração é " + subtração + " daleeeeeeeeee gremio");
+
+let multiplicação = num1 * num2;
+console. log("O valor da multiplicação é " + multiplicação + " daleeeeeeeeee gremio");
+
+let divisão = num1 / num2;
+console. log("O valor da divisão é " + divisão + " daleeeeeeeeee gremio");
+
